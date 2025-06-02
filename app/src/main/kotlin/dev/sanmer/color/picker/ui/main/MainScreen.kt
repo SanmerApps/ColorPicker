f8558ea4d8754d19a549256c728c99e9eef188ef
SanmerDev <sanmer@sanmer.dev>
2025-06-02 15:50:10 +0800

    Opt-in `ExperimentalLayoutApi` in `ComposeConventionPlugin`

--- app/src/main/kotlin/dev/sanmer/color/picker/ui/main/MainScreen.kt
+++ app/src/main/kotlin/dev/sanmer/color/picker/ui/main/MainScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -107,7 +106,6 @@ fun MainScreen(
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 private fun ButtonsItem(
     importJson: (Uri) -> Unit,
