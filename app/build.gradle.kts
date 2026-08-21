731593de91a18fc351fb470e85e4da78fdd70dd2
SanmerDev <sanmer@sanmer.dev>
2026-08-21 19:13:45 +0800

    Fix dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core)
     implementation(libs.androidx.core.splashscreen)
